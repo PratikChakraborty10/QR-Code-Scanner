a7402ede7c50fe6487494a2614139057718c87f6
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2021-12-25 15:53:05 +0530

    minor testing

--- app/src/main/java/in/pratikchakraborty/qrcodescanner/MainActivity.kt
+++ app/src/main/java/in/pratikchakraborty/qrcodescanner/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.ClipboardManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -13,6 +14,7 @@ import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.*
